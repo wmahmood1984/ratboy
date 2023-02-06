eb7deb5145d2363ddfea3bf19a7e6ad98d3bcc92
blockchainer2022 <91312709+blockchainer2022@users.noreply.github.com>
2023-02-06 19:30:57 +0530

    added token_create_success page

--- src/pages/LockRecordDetails/index.js
+++ src/pages/LockRecordDetails/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Layout } from "../../components";
 import ListItem from "../../components/listItem";
 import { shortAddress } from "../../helpers";
-import { FiChevronRight } from "react-icons/fi";
+// import { FiChevronRight } from "react-icons/fi";
 import { Collapse } from "@mui/material";
+import Timer from "./Timer";
 const LockRecordDetails = () => {
-  const {state} = useLocation()
+  const { state } = useLocation();
   const [show, setShow] = useState(false);
   const head = ["Unlock #", "Time (UTC)", "Unlocked tokens"];
   const data = [
@@ -24,15 +25,19 @@ const LockRecordDetails = () => {
   return (
     <Layout>
       <div className=" px-6 mt-28  mb-20 ">
+        <div className="bg-white dark:bg-dark-400 border dark:border-lightDark  p-6 rounded-md shadow grid grid-cols-1  mb-6">
+          <p className=" font-semibold text-lg text-center text-primary-400 ">
+            Unlock In
+          </p>
+          <Timer />
+        </div>{" "}
         <div className="bg-white dark:bg-dark-400 border dark:border-lightDark   rounded-md shadow grid grid-cols-1  ">
           <p className="p-4  border-b dark:border-lightDark">Token Info</p>
           <div className="p-4">
             <div className="grid gap-y-4 mt-4">
               <ListItem
                 title={"Token Address"}
-                desc={shortAddress(
-                  state.token
-                )}
+                desc={shortAddress(state.token)}
                 color={"primary"}
               />{" "}
               <ListItem title={"Token Name"} desc={`${state.name}`} />
@@ -46,15 +51,22 @@ const LockRecordDetails = () => {
           <div className="p-4">
             <div className="grid gap-y-4 mt-4">
               <ListItem
-                title={"Token Address"}
-                desc={shortAddress(
-                  state.token
-                )}
+                title={"Total Amount Locked"}
+                desc={"55,555 MOONDAY"}
                 color={"primary"}
               />{" "}
-              <ListItem title={"Token Name"} desc={`${state.name}`} />
-              <ListItem title={"Token Symbol"} desc={`${state.symbol}`} />
-              {/* <ListItem title={"Token Decimal"} desc={"18"} /> */}
+              <ListItem title={"Total Values Locked"} desc={`0`} />
+              <ListItem
+                title={"Owner"}
+                desc={shortAddress(
+                  "0x09b754BA0f822a3C02733cbF691C65fa9a996Bdc"
+                )}
+              />
+              <ListItem title={"Lock Date"} desc={"2023.02.06 08:43 UTC"} />{" "}
+              <ListItem
+                title={"Unlock Date"}
+                desc={"2025.02.06 07:47 UTC (in 2 years)"}
+              />
               {/* <div className="text-xs sm:text-sm flex justify-between pb-2 items-center border-b border-lightDark">
                 <p className=" ">{"Vesting Info"}</p>
                 <button
---
+++ src/pages/tokenCreateSuccess/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Layout } from "../../components";
+// import ListItem from "../../components/listItem";
+import { shortAddress } from "../../helpers";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-hot-toast";
+const TokenCreateSuccess = () => {
+  const token = "0xE769D4bA7FbE3890427a577C96714429c91210BF";
+  const data = [
+    {
+      title: "Name",
+      data: "Mytoken",
+    },
+    {
+      title: "Symbol",
+      data: "MT",
+    },
+    {
+      title: "Total Supply",
+      data: "10,000,000,000",
+    },
+    {
+      title: "Address",
+      data: (
+        <span>
+          <span className="md:hidden">{shortAddress(token)} </span>
+          <span
+            className=" hidden md:inline-block
+          "
+          >
+            {token}
+          </span>
+        </span>
+      ),
+    },
+  ];
+  const copyToClipBoard = async (copyMe) => {
+    try {
+      await navigator.clipboard.writeText(copyMe);
+      toast.success("Copied!");
+    } catch (err) {
+      toast.error("Failed to copy!");
+    }
+  };
+  return (
+    <Layout>
+      <div className="px-4 pb-10 pt-24 grid grid-cols-1 gap-y-4">
+        <div className="bg-white md:col-span-5 dark:bg-dark-400 border dark:border-lightDark rounded-md shadow-xl p-4 py-10 px-6">
+          <p className="text-green-400 text-center text-xl">
+            Your token was created!
+          </p>
+          <table className="w-full mt-5">
+            <tbody>
+              {data.map((item, index) => (
+                <tr key={index} className="border-b dark:border-lightDark">
+                  <td className="py-3 pr-4">{item.title}</td>
+                  <td className="text-primary-400 py-3 pr-4">{item.data}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex flex-col md:flex-row  justify-center items-center mt-5">
+            <button className="bg-primary-400 md:w-auto w-full px-10 py-1.5 sm:py-2 rounded-md">
+              View Transaction
+            </button>
+            <button
+              onClick={() => copyToClipBoard(token)}
+              className="bg-primary-400 md:w-auto w-full px-10 py-1.5 sm:py-2 my-4 md:my-0 md:ml-2 rounded-md"
+            >
+              Copy Address
+            </button>{" "}
+            <button className="bg-primary-400 md:w-auto w-full px-10 py-1.5 sm:py-2 md:ml-2 rounded-md">
+              Create LaunchPad
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default TokenCreateSuccess;
