4e1762f46ba8fc3456c94444cb50c3507d909835
blockchainer2022 <91312709+blockchainer2022@users.noreply.github.com>
2023-02-10 16:51:51 +0530

    added contributor list export button

--- src/pages/projectPreview/components/Information.js
+++ src/pages/projectPreview/components/Information.js
@@ -1,8 +1,9 @@
 import { formatEther } from "ethers/lib/utils";
 import React from "react";
 import ListItem from "../../../components/listItem";
+import { CSVLink, CSVDownload } from "react-csv";
 
-const Information = ({data}) => {
+const Information = ({ data }) => {
   const informationList = [
     {
       title: "Status",
@@ -23,7 +24,16 @@ const Information = ({data}) => {
       desc: "0.1 BNB",
     },
   ];
-
+  const contributorList = [
+    {
+      address: "abc",
+      value: 0.01,
+    },
+    {
+      address: "acx",
+      value: 0.02,
+    },
+  ];
   return (
     <div className="bg-white md:col-span-5 dark:bg-dark-400 border dark:border-lightDark rounded-md shadow-xl">
       <div className=" border-b  border-lightDark px-4 py-4">
@@ -31,20 +41,26 @@ const Information = ({data}) => {
       </div>
       <div className="p-4 sm:p-6 ">
         <div className="grid gap-y-4 mt-4">
-
-            <React.Fragment >
-              <ListItem title={"Status"} desc={"Incoming"} color={"primary"} />
-            </React.Fragment>
-            <React.Fragment >
-              <ListItem title={"Sale Type"} desc={"Public"} color={"parimary"} />
-            </React.Fragment>
-            <React.Fragment >
-              <ListItem title={"Max Buy"} desc={`${formatEther(data[4][2])} ${window.ethereum?.networkVersion == 97 ? "BNB" : "ETH"}`} />
-            </React.Fragment>
-            <React.Fragment >
-              <ListItem title={"Minimum Buy"}desc={`${formatEther(data[4][3])} ${window.ethereum?.networkVersion == 97 ? "BNB" : "ETH"}`} />
-            </React.Fragment>
-
+          <ListItem title={"Status"} desc={"Incoming"} color={"primary"} />
+          <ListItem title={"Sale Type"} desc={"Public"} color={"parimary"} />
+          <ListItem
+            title={"Max Buy"}
+            desc={`${formatEther(data[4][2])} ${
+              window.ethereum?.networkVersion == 97 ? "BNB" : "ETH"
+            }`}
+          />
+          <ListItem
+            title={"Minimum Buy"}
+            desc={`${formatEther(data[4][3])} ${
+              window.ethereum?.networkVersion == 97 ? "BNB" : "ETH"
+            }`}
+          />{" "}
+          <ListItem title={"Total Contributors"} desc={`0`} />
+          <CSVLink data={contributorList} filename="contributor_list">
+            <button className=" text-white rounded-lg py-2.5  bg-primary-400 w-full text-sm font-bold">
+              Export Contributor List
+            </button>
+          </CSVLink>
         </div>
       </div>
     </div>
